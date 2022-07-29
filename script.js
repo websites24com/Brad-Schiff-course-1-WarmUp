ad27a44982813f883eb33fc3599e86a66b9b6be7
Piotr <websites@hello-24.com>
2022-07-28 22:37:09 -0500

    Arrays, Functions, Scope, Context - basic things

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ cat.click();
 */
 
 /*** Arrays ***/
-
+/*
 let myFavoriteNumbers = [9, 2, 4, 5, 6];
 let myWords = ["red", "orange", "green"];
 
@@ -71,3 +71,119 @@ myWords.splice(0, 1);
 console.log(myWords);
 
 console.log(myPets[1].species);
+*/
+
+/*** Making decisions ***/
+/*
+let strawberryCount = 2;
+
+if (strawberryCount > 9) {
+  console.log("Congrats");
+} else {
+  console.log("Sorry, we do not ship orders that small");
+}
+
+while (strawberryCount <= 200) {
+  console.log("There are: " + strawberryCount + " strawberries");
+  strawberryCount++;
+}
+*/
+
+/*** Higher-order functions */
+
+// a) Accepts a function as an argument
+// b) Returns a function
+
+/*
+function ourAmazingFunction() {
+  alert("thank you for clicking");
+}
+
+document.addEventListener("click", ourAmazingFunction);
+
+function doubleMe(x) {
+  return x * 2;
+}
+
+console.log(doubleMe(20));
+
+function createMultiplier(multiplier) {
+  return function (x) {
+    return x * multiplier;
+  };
+}
+
+doubleMe = createMultiplier(2);
+let tripleMe = createMultiplier(3);
+let quadrupleMe = createMultiplier(4);
+console.log(doubleMe(5), tripleMe(6), quadrupleMe(7));
+
+function saySthNice(color) {
+  console.log("The color " + color + " is my favorite");
+}
+
+let myColors = ["red", "orange", "yellow"];
+myColors.forEach(saySthNice);
+
+// ignore
+
+let pets = [
+  { name: "Meowsalot", species: "cat", age: 2 },
+  { name: "Barksalot", species: "dog", age: 3 },
+  { name: "Purrsloud", species: "cat", age: 8 },
+];
+
+pets.push({ name: "Puppster", species: "dog", age: 1 });
+
+let onlyDogs = pets
+  .filter((dog) => dog.species === "dog")
+  .map((baby) => baby.age < 3);
+
+console.log(onlyDogs);
+*/
+
+/*** Scope and context ***/
+// Direction from inside to outside NOT from outside to inside!
+// Scope is about variables, Context is about objects
+/*
+
+// Scope
+
+let myName = "Brad";
+
+function amazingFunction() {
+  let myName = "Brad Junior";
+  if (2 + 2 == 4) {
+    let myName = "Brad the 3rd";
+    console.log("inside the if statement", myName);
+  }
+  console.log("inside our function", myName);
+}
+
+amazingFunction();
+console.log("in the global scope", myName);
+*/
+
+// Context
+
+let john = {
+  firstName: "John",
+  lastName: "Doe",
+  driveCar() {
+    function imFunctionNotAMethod() {
+      console.log(this);
+    }
+    imFunctionNotAMethod();
+    console.log(this.firstName + " " + this.lastName + " is driving a car.");
+  },
+};
+
+john.driveCar();
+
+function breathe() {
+  console.log(
+    this.firstName + " " + this.lastName + " just inhaled and exhaled."
+  );
+}
+
+breathe.call(john);
